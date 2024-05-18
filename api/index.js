e6e73d9a7ad0a4067d92cfefc230d19d8e715adf
furPedroRibeiro <pedrocodejob@gmail.com>
2024-05-18 17:57:00 -0300

    atualização final

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json())
 
 app.get('/funcionamento', (req, res) => {
-  res.set('Access-Control-Allow-Origin', 'https://portaomatadouro.vercel.app');
+  res.set('Access-Control-Allow-Origin', 'https://portaomatadouro.vercel.app/');
   let status
   try {
     const data = fs.readFileSync('./status.json', 'utf-8')
@@ -30,7 +30,7 @@ app.get('/funcionamento', (req, res) => {
 })
 
 app.post('/funcionamento', (req, res) => {
-  res.set('Access-Control-Allow-Origin', 'https://portaomatadouro.vercel.app');
+  res.set('Access-Control-Allow-Origin', 'https://portaomatadouro.vercel.app/');
   const filePath = './status.json'
   const encoding = 'utf-8'
   const reqStatus = req.body.funcionando
